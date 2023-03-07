80ca9e446cc3925c69fb3e0e104b410122bedffe
root <root@ip-172-31-41-48.ap-south-1.compute.internal>
2023-03-07 13:39:42 +0000

    variables note

---
+++ tera-variables/main.tf
@@ -0,0 +1,24 @@
+resource "local_file" "devops"{
+filename = var.file_list[0]	
+content = var.content_map["content1"]
+}
+
+resource "local_file" "devops-var" {
+	filename = var.file_list[1] 
+	content = var.content_map["content2"]
+}
+
+output "devoops_op_trainer" {
+value = var.devops_op_trainer
+}
+
+output "aws_ec2_instances" {
+value = var.aws_ec2_object
+}
+
+
+
+output "tf_batch_students" {
+value = var.no_of_students
+}
+
